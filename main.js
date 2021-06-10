3b8594f4036a2fda88186c2a7d0bd4e97b278b92
bora12344 <deborahlee920411@gmail.com>
2021-06-10 16:39:24 -0500

    JS: Navbar menu click

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 'use strict'
 
 
-//Make navbar transparent when it is on the top 
+// Make navbar transparent when it is on the top 
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 
@@ -14,4 +14,22 @@ document.addEventListener('scroll', () => {
     } else {
         navbar.classList.remove('navbar--dark');
     }
+});
+
+// Handle scrolling when tapping on the navar menu 
+// 보통 click 을 하게 되면 옆에 event 가 같이 들어옴
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    
+
+// navbar menu 를 눌렀을 때 undefined 안나오게 하기
+    const target = event.target;
+    const link = target.dataset.link;
+
+    if (link == null){
+        return;
+    }
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: "smooth"}); 
 });
