93006abda1ad096c4fa858ea13249bcc8b25f380
bora12344 <deborahlee920411@gmail.com>
2021-06-16 11:28:17 -0500

    JS: arrow-up

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
     if (window.scrollY > navbarHeight) {
-        navbar.classList.add('navbar--dark')
+        navbar.classList.add('navbar--dark');
     } else {
         navbar.classList.remove('navbar--dark');
     }
@@ -36,6 +36,21 @@ const home = document.querySelector('.home__container');
 const homeHeight = home.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
     home.style.opacity = 1 - window.scrollY / homeHeight;
+});
+
+// show arrow-up button when scrolling down
+const arrowUp = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+    if(window.scrollY > homeHeight /2) {
+        arrowUp.classList.add('visible');
+    }  else {
+        arrowUp.classList.remove('visible');
+    }
+});
+
+// Handle click on the "arrow-up" button 
+arrowUp.addEventListener('click', () => {
+    scrollIntoView('#home');
 });
 
 
